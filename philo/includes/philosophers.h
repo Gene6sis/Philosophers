298ec9182219910230753e98da785f74148f892d
Adam BEN MCHICHI <adben-mc@e2r4p12.clusters.42paris.fr>
2022-02-15 17:41:01 +0100

    Makefile and lib

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pushswap.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adben-mc <adben-mc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/02 01:04:55 by adben-mc          #+#    #+#             */
+/*   Updated: 2022/02/15 17:37:06 by adben-mc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include "../ft_printf/ft_printf.h"
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
